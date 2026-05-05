f0272694adf9297e9b41ee6d618e4b3ee03269dc
amir ap <arabpour.amir@gmail.com>
2026-05-05 11:11:10 -0400

    post domain files refactored completely, ready for Day 7 of the plan

--- backend/app/services/post_service.py
+++ backend/app/services/post_service.py
@@ -6,6 +6,11 @@ from app.repositories import post_repository, company_repository
 from app.schemas.posts import PostCreate, PostStatusUpdate, PostUpdate, PostFilter
 
 
+POST_NOT_FOUND_404 = "post not found"
+COMPANY_NOT_FOUND_404 = "company not found"
+NO_FIELDS_TO_UPDATE_400 = "no fields to update"
+
+
 def get_all(db: Session, filters: PostFilter) -> list[Post]:
     return post_repository.get_all(db, filters)
 
@@ -19,26 +24,23 @@ def create(db: Session, data: PostCreate) -> Post:
     return post_repository.create(db, data)
 
 
-def update_status(db: Session, post_id: int, data:PostStatusUpdate) -> Post:
+def update_status(db: Session, post_id: int, data: PostStatusUpdate) -> Post:
     post = _get_post_or_fail(db, post_id)
     return post_repository.update_status(db, post, data)
 
 
 def update(db: Session, post_id: int, data: PostUpdate) -> Post:
     post = _get_post_or_fail(db, post_id)
-    
+
     data_dict = data.model_dump(exclude_unset=True)
     if not data_dict:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="fields can't be empty"
+            detail=NO_FIELDS_TO_UPDATE_400,
         )
 
-
-    if "company_id" in data_dict:
-        company_id = _get_company_id_or_fail(data_dict["company_id"])        
-        if post.company_id != company_id: 
-            _check_company_exists_or_fail(db, company_id)
+    if data.company_id is not None and post.company_id != data.company_id:
+        _check_company_exists_or_fail(db, data.company_id)
 
     return post_repository.update(db, post, data)
 
@@ -52,26 +54,19 @@ def _get_post_or_fail(db: Session, post_id: int) -> Post:
     post = post_repository.get_by_id(db, post_id)
 
     if post is None:
-       raise HTTPException(
+        raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="post not found"
+            detail=POST_NOT_FOUND_404,
         )
+
     return post
 
-
-def _get_company_id_or_fail(company_id: int | None) -> int:
-    if company_id is None:
-        raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="company can't be none"
-            )
-    return company_id
 
 def _check_company_exists_or_fail(db: Session, company_id: int) -> None:
     company = company_repository.get_by_id(db, company_id)
 
     if company is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
-            detail="company not found"
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=COMPANY_NOT_FOUND_404,
         )
